004e214d3a3b75bfc38313e6961c41e59928f275
mvaskuri <mani.vaskuri@gmail.com>
2025-08-11 19:28:49 +0530

    updated terraform.tfvars

--- variables.tf
+++ variables.tf
@@ -1,11 +1,9 @@
 variable "aws_region" {
     description = "The region to deploy the resources"
     type = string
-    default = "us-east-1"
 }
 
 variable "vpc_cidr" {
     description = "The CIDR Range for the VPC"
     type = string
-    default = "10.0.0.0/16"
 }
